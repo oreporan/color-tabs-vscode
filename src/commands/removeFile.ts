0bd8e13a8ad1ed53a221f3510a7a6a6bf7b5a6cf
Ore Poran <oreporan@gmail.com>
2020-08-30 16:46:08 +0300

    fix remove api

--- src/commands/removeFile.ts
+++ src/commands/removeFile.ts
@@ -1,15 +1,15 @@
 import * as vscode from "vscode";
 import getSettings from '../getSettings';
 
-const removeFile = () => {
+const removeFile = async () => {
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
   
   const settings = vscode.workspace.getConfiguration("colorTabs");
   const currentConfig = getSettings().config || []
-  const path = vscode.workspace.asRelativePath(editor.document.uri.fsPath);
+  const quickPickResults = await vscode.window.showQuickPick(currentConfig.map(x => x.regex), {canPickMany: true, placeHolder: 'ColorTabs configurations to remove'}) || []
 
-  const filteredConfig = currentConfig.filter(x => x.regex !== path)
+  const filteredConfig = currentConfig.filter(x => quickPickResults.every(y => x.regex !== y))
 
   settings.update(
     'config',
