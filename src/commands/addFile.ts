39c747bba1656ca4e7c0187949b15027c3f06559
Ore Poran <oreporan@gmail.com>
2020-08-30 16:11:11 +0300

    allow you to choose regex

--- src/commands/addFile.ts
+++ src/commands/addFile.ts
@@ -1,17 +1,26 @@
 import * as vscode from "vscode";
-import getSettings from '../getSettings';
+import getSettings from "../getSettings";
+import * as path from "path";
 
-const addFile = () => {
+const addFile = async () => {
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
-  
+
   const settings = vscode.workspace.getConfiguration("colorTabs");
-  const currentConfig = getSettings().config || []
-  const path = vscode.workspace.asRelativePath(editor.document.uri.fsPath);
+  const relativePath = vscode.workspace.asRelativePath(
+    editor.document.uri.fsPath
+  );
+  const currentConfig = getSettings().config || [];
+  const dir = `${path.parse(relativePath).dir}${path.sep}.*`;
+
+  const value = await vscode.window.showInputBox({
+    value: dir,
+    prompt: "Edit the folder regex",
+  });
 
   settings.update(
-    'config',
-    [...currentConfig, {regex: path}],
+    "config",
+    [...currentConfig, { regex: value }],
     vscode.ConfigurationTarget.Workspace
   );
 };
